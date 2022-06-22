9ea86f46f0db3420b11b7c2c8be05985cbd31e08
Tingting Duan <duantingting978@gmail.com>
2022-06-22 17:52:21 -0400

    init

---
+++ kywapi/models/wage.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Wage(models.Model):
+
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    salary = models.IntegerField()
+    city = models.CharField(max_length=200)
+    work_state = models.CharField(max_length=50)
+    year = models.IntegerField()
+    source = models.CharField(max_length=200)
